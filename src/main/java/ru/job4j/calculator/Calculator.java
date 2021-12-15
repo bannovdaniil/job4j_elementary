a792846791c042bfd1ca5a4ba0bd5fd5da3ecff8
Daniil Bannov#14 <baddan@mail.ru>
2021-12-15 13:38:31 +0300

    3. Арифметические операции. [#185 #240128]

--- src/main/java/ru/job4j/calculator/Calculator.java
+++ src/main/java/ru/job4j/calculator/Calculator.java
@@ -1,7 +1,7 @@
 package ru.job4j.calculator;
 
 public class Calculator {
-    public static void main(String[] args) {
+        public static void main(String[] args) {
         int six = 6;
         int four = 4;
         int five = 5;
