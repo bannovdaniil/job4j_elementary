0b2f56586ecb7f3ed0f97c658d7d80c6b6d2f58b
Daniil Bannov#14 <baddan@mail.ru>
2021-12-17 12:45:22 +0300

    5. Конвертер валюты [#41583 #240280]

--- src/main/java/ru/job4j/StudentInfo.java
+++ src/main/java/ru/job4j/StudentInfo.java
@@ -6,4 +6,19 @@ public class StudentInfo {
         System.out.println("10.07.1977");
 
     }
+
+    public static class ArgMethod {
+        public static void hello(String name, int age) {
+            System.out.println("Hello, " + name + ", age = " + age);
+        }
+
+        public static void main(String[] args) {
+            String name = "Job4j";
+            int age = 6;
+            ArgMethod.hello(name, age);
+            ArgMethod.hello(name, age);
+            ArgMethod.hello(name, age);
+            ArgMethod.hello(name, age);
+        }
+    }
 }
